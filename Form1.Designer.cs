805cb846d117d818676d973e4e1dcd89a97906fb
SevvalBerfin <sevvalberfinozbaltaci@gmail.com>
2026-05-16 16:57:09 +0300

    Veri tabanindeki student tablosunu class yapisiyle tanimlama

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace yurt_otomasyon_projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_list = new System.Windows.Forms.Button();
+            this.btn_guncelle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_isim = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_isim = new System.Windows.Forms.TextBox();
+            this.txt_soyisim = new System.Windows.Forms.TextBox();
+            this.txt_tc = new System.Windows.Forms.TextBox();
+            this.txt_mail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_telefon = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1210, 178);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(778, 208);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(75, 23);
+            this.btn_ekle.TabIndex = 1;
+            this.btn_ekle.Text = "EKLE";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            // 
+            // btn_list
+            // 
+            this.btn_list.Location = new System.Drawing.Point(778, 260);
+            this.btn_list.Name = "btn_list";
+            this.btn_list.Size = new System.Drawing.Size(75, 23);
+            this.btn_list.TabIndex = 2;
+            this.btn_list.Text = "LİSTELE";
+            this.btn_list.UseVisualStyleBackColor = true;
+            // 
+            // btn_guncelle
+            // 
+            this.btn_guncelle.Location = new System.Drawing.Point(778, 322);
+            this.btn_guncelle.Name = "btn_guncelle";
+            this.btn_guncelle.Size = new System.Drawing.Size(75, 23);
+            this.btn_guncelle.TabIndex = 3;
+            this.btn_guncelle.Text = "GÜNCELLE";
+            this.btn_guncelle.UseVisualStyleBackColor = true;
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Location = new System.Drawing.Point(778, 375);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(75, 23);
+            this.btn_sil.TabIndex = 4;
+            this.btn_sil.Text = "SİL";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_telefon);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txt_mail);
+            this.groupBox1.Controls.Add(this.txt_tc);
+            this.groupBox1.Controls.Add(this.txt_soyisim);
+            this.groupBox1.Controls.Add(this.txt_isim);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lbl_isim);
+            this.groupBox1.Location = new System.Drawing.Point(12, 187);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(682, 408);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // lbl_isim
+            // 
+            this.lbl_isim.AutoSize = true;
+            this.lbl_isim.Location = new System.Drawing.Point(32, 48);
+            this.lbl_isim.Name = "lbl_isim";
+            this.lbl_isim.Size = new System.Drawing.Size(34, 16);
+            this.lbl_isim.TabIndex = 0;
+            this.lbl_isim.Text = "İsim:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyisim:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TC:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "E-mail:";
+            // 
+            // txt_isim
+            // 
+            this.txt_isim.Location = new System.Drawing.Point(145, 48);
+            this.txt_isim.Name = "txt_isim";
+            this.txt_isim.Size = new System.Drawing.Size(446, 22);
+            this.txt_isim.TabIndex = 4;
+            // 
+            // txt_soyisim
+            // 
+            this.txt_soyisim.Location = new System.Drawing.Point(145, 100);
+            this.txt_soyisim.Name = "txt_soyisim";
+            this.txt_soyisim.Size = new System.Drawing.Size(446, 22);
+            this.txt_soyisim.TabIndex = 5;
+            // 
+            // txt_tc
+            // 
+            this.txt_tc.Location = new System.Drawing.Point(145, 156);
+            this.txt_tc.Name = "txt_tc";
+            this.txt_tc.Size = new System.Drawing.Size(446, 22);
+            this.txt_tc.TabIndex = 6;
+            this.txt_tc.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txt_mail
+            // 
+            this.txt_mail.Location = new System.Drawing.Point(145, 232);
+            this.txt_mail.Name = "txt_mail";
+            this.txt_mail.Size = new System.Drawing.Size(446, 22);
+            this.txt_mail.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 312);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Telefon:";
+            // 
+            // txt_telefon
+            // 
+            this.txt_telefon.Location = new System.Drawing.Point(145, 312);
+            this.txt_telefon.Name = "txt_telefon";
+            this.txt_telefon.Size = new System.Drawing.Size(446, 22);
+            this.txt_telefon.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 607);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_guncelle);
+            this.Controls.Add(this.btn_list);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button btn_list;
+        private System.Windows.Forms.Button btn_guncelle;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_mail;
+        private System.Windows.Forms.TextBox txt_tc;
+        private System.Windows.Forms.TextBox txt_soyisim;
+        private System.Windows.Forms.TextBox txt_isim;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_isim;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_telefon;
+    }
+}
+
---
+++ student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace yurt_otomasyon_projesi
+{
+    [Table("Table_student")]
+    internal class student
+    {
+        [Key]
+        public int student_ıd { get; set; }
+
+        [Required]
+        [MaxLength (50)]
+        public string student_name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string student_surname { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        public string student_tc { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string student_email { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        public string student_phone { get; set; }
+    }
+}
